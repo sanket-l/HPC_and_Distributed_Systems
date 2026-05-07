571a51ff40402bdb0c215c4bac8a77e8d5bef39c
Sanket Landge <sanket@sanket-macbook.local>
2026-05-07 16:27:48 -0700

    hw5 code

--- hw5_transpose_matrix/template/transpose.c
+++ hw5_transpose_matrix/template/transpose.c
@@ -1,16 +1,43 @@
 #include "util.h"
 
-
-void
-mat_sqtrans_seq(Mat *mat)
+void mat_sqtrans_seq(Mat *mat)
 {
-    // your code goes here
-    return;
+    unsigned int n = mat->n;
+
+    for (unsigned int i = 0; i < n; i++)
+    {
+        for (unsigned int j = i + 1; j < n; j++)
+        {
+            double temp = mat->ptr[i * n + j];
+            mat->ptr[i * n + j] = mat->ptr[j * n + i];
+            mat->ptr[j * n + i] = temp;
+        }
+    }
 }
 
-void
-mat_sqtrans_par(Mat *mat, unsigned int grain, unsigned int threads)
+void mat_sqtrans_par(Mat *mat, unsigned int grain, unsigned int threads)
 {
-    // your code goes here
-    return;
+    unsigned int n = mat->n;
+
+    unsigned int total = n * (n - 1) / 2;
+
+#pragma omp parallel for num_threads(threads) schedule(dynamic, grain)
+    for (unsigned int k = 0; k < total; k++)
+    {
+
+        unsigned int tmp = k;
+        unsigned int i = 0;
+
+        while (tmp >= (n - i - 1))
+        {
+            tmp -= (n - i - 1);
+            i++;
+        }
+
+        unsigned int j = i + 1 + tmp;
+
+        double temp = mat->ptr[i * n + j];
+        mat->ptr[i * n + j] = mat->ptr[j * n + i];
+        mat->ptr[j * n + i] = temp;
+    }
 }
